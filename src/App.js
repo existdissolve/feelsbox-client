08d86d9dc52e6d9691391652c12c1d6c4c39841d
Joel Watson <existdissolve@gmail.com>
2018-03-31 23:29:24 -0500

    Support category

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ import './App.css';
 
 var app = firebase.initializeApp(config);
 
+const categoryLabels = {
+    food: 'Food',
+    drink: 'Drink',
+    event: 'Holidays and Events',
+    misc: 'Miscellaneous'
+}
 
 function onSignIn (user) {
 
@@ -48,6 +54,74 @@ class App extends Component {
         });
     }
 
+    renderCategory(categoryKey, category) {
+        const keys = Object.keys(category);
+        const len = keys.length;
+        let rows = Math.floor(len / 3);
+        rows = (len % 3) !== 0 ? rows + 1 : rows + 0;
+        const groups = {};
+
+        for (let i = 0; i < rows; i++) {
+            for (let x = 0; x < 3; x++) {
+                const position = (i * 3) + x;
+                const value = category[keys[position]];
+
+                if (value) {
+                    if (!groups[i]) {
+                        groups[i] = {};
+                    }
+
+                    groups[i][keys[position]] = value;
+                } else {
+                    break;
+                }
+            }
+        }
+
+        return (
+            <div key={categoryKey}>
+                <div className="category">{categoryLabels[categoryKey]}</div>
+                {Object.keys(groups).map(groupKey => {
+                    const group = groups[groupKey];
+
+                    return (
+                        <div className="row" key={groupKey}>
+                            {Object.keys(group).map(emojiKey => {
+                                const emoji = group[emojiKey];
+                                const grid = new Array(8).fill(true);
+                                return (
+                                    <div className="col" key={emojiKey}>
+                                        <a className="emoji" onClick={this.onClick.bind(this, emojiKey)}>
+                                            {grid.map((line, index) => {
+                                                const row = new Array(8).fill(true);
+                                                return (
+                                                    <span className="pixel-row" key={index}>
+                                                        {row.map((pixel, idx) => {
+                                                            let bgColor = '000000';
+                                                            const pixelIndex = ((index * 8) + idx) + 1;
+
+                                                            if (emoji.pixels[pixelIndex]) {
+                                                                bgColor = emoji.pixels[pixelIndex].c;
+                                                            }
+
+                                                            return (
+                                                                <span className="pixel" key={idx} style={{backgroundColor: `#${bgColor}`}}></span>
+                                                            );
+                                                        })}
+                                                    </span>
+                                                );
+                                            })}
+                                        </a>
+                                    </div>
+                                );
+                            })}
+                        </div>
+                    );
+                })}
+            </div>
+        )
+    }
+
     render () {
         const {loggedIn, data} = this.state;
 
@@ -65,28 +139,18 @@ class App extends Component {
             );
         }
 
-        const keys = Object.keys(data);
-        const len = keys.length;
-        let rows = Math.floor(len / 3);
-        rows = (len % 3) !== 0 ? rows + 1 : rows + 0;
-        const groups = {};
+        const categories = {};
 
-        for (let i = 0; i < rows; i++) {
-            for (let x = 0; x < 3; x++) {
-                const position = (i * 3) + x;
-                const value = data[keys[position]];
+        Object.keys(data).forEach(key => {
+            const item = data[key];
+            const category = item.category;
 
-                if (value) {
-                    if (!groups[i]) {
-                        groups[i] = {};
-                    }
-
-                    groups[i][keys[position]] = value;
-                } else {
-                    break;
-                }
+            if (!categories[category]) {
+                categories[category] = {};
             }
-        }
+
+            categories[category][key] = item;
+        });
 
         return (
             <div className="App">
@@ -95,42 +159,8 @@ class App extends Component {
                 </header>
                 <div className="section container">
                     <h2>Emojis</h2>
-                    {Object.keys(groups).map(groupKey => {
-                        const group = groups[groupKey];
-
-                        return (
-                            <div className="row" key={groupKey}>
-                                {Object.keys(group).map(emojiKey => {
-                                    const emoji = group[emojiKey];
-                                    const grid = new Array(8).fill(true);
-                                    return (
-                                        <div className="col" key={emojiKey}>
-                                            <a className="emoji" onClick={this.onClick.bind(this, emojiKey)}>
-                                                {grid.map((line, index) => {
-                                                    const row = new Array(8).fill(true);
-                                                    return (
-                                                        <span className="pixel-row" key={index}>
-                                                            {row.map((pixel, idx) => {
-                                                                let bgColor = '000000';
-                                                                const pixelIndex = ((index * 8) + idx) + 1;
-
-                                                                if (emoji[pixelIndex]) {
-                                                                    bgColor = emoji[pixelIndex].c;
-                                                                }
-
-                                                                return (
-                                                                    <span className="pixel" key={idx} style={{backgroundColor: `#${bgColor}`}}></span>
-                                                                );
-                                                            })}
-                                                        </span>
-                                                    );
-                                                })}
-                                            </a>
-                                        </div>
-                                    );
-                                })}
-                            </div>
-                        );
+                    {Object.keys(categories).map(categoryKey => {
+                        return this.renderCategory(categoryKey, categories[categoryKey]);
                     })}
                 </div>
                 <div className="section container">
@@ -140,6 +170,15 @@ class App extends Component {
                             <div className="form-group">
                                 <label>Name:</label>
                                 <input type="text" name="emoji" className="form-control" />
+                            </div>
+                            <div className="form-group">
+                                <label>Category:</label>
+                                <select name="category" className="form-control">
+                                    <option value="misc">Miscellaneous</option>
+                                    <option value="food">Food</option>
+                                    <option value="drink">Drinks</option>
+                                    <option value="holiday">Holiday</option>
+                                </select>
                             </div>
                             <div className="form-group">
                                 <label>File:</label>
