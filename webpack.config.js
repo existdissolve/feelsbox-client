34b047ce249b38315f0ffece5c782f6e8dae01ba
Joel Watson <existdissolve@gmail.com>
2018-05-06 12:05:45 -0500

    Different approach for manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 const htmlPlugin = new HtmlWebPackPlugin({
     template: "./src/index.html",
     filename: "./index.html"
 });
 
-const copyPlugin = new CopyWebpackPlugin([{
+/*const copyPlugin = new CopyWebpackPlugin([{
     from: './favicon.ico',
     to: './favicon.ico',
     toType: 'file'
@@ -19,7 +20,17 @@ const copyPlugin = new CopyWebpackPlugin([{
     from: './manifest.json',
     to: './manifest.json',
     toType: 'file'
-}]);
+}]);*/
+const manifestPlugin = new WebpackPwaManifest({
+    name: 'FeelsBox',
+    short_name: 'FeelsBox',
+    description: 'A box of feels, just for you!',
+    background_color: '#ffffff',
+    icons: [{
+        src: path.resolve('./feelsbox.png'),
+        sizes: [128, 96, 64, 32, 24, 16] // multiple sizes
+    }]
+});
 
 module.exports = {
     module: {
@@ -31,7 +42,7 @@ module.exports = {
             }
         }]
     },
-    plugins: [htmlPlugin, copyPlugin],
+    plugins: [htmlPlugin, manifestPlugin],
     resolve: {
         alias: {
             '-': path.join(__dirname, 'src')
