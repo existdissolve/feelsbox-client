7e57267d9da95dcc1438baf4bbc346cdc92f265b
existdissolve <existdissolve@gmail.com>
2020-10-30 14:00:29 -0500

    More fixes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const path = require('path');
 const open = require('open');
 const compression = require('compression');
 const favicon = require('serve-favicon');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
 /*eslint-disable no-console */
 
@@ -11,7 +12,12 @@ const app = express();
 
 app.use(compression());
 app.use(express.static('build'));
-//app.use(favicon(path.join(__dirname,'assets','dist','favicon.ico')));
+app.use(favicon(path.join(__dirname,'assets','dist','favicon.ico')));
+
+app.use('/api/graphql', createProxyMiddleware({
+    target: 'https://feelsbox-server-v2.herokuapp.com',
+    changeOrigin: true
+}));
 
 app.get('*', function(req, res) {
     res.sendFile(path.join(__dirname, '../build/index.html'));
@@ -20,7 +26,5 @@ app.get('*', function(req, res) {
 app.listen(port, function(err) {
     if (err) {
         console.log(err);
-    } else {
-        open(`http://localhost:${port}`);
     }
 });
