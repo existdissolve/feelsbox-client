a91f36d3645ad6b636f272dfa21de93c743084b5
existdissolve <existdissolve@gmail.com>
2020-10-30 08:41:47 -0500

    Fix endpoint

--- src/graphql/client.js
+++ src/graphql/client.js
@@ -2,10 +2,13 @@ import {ApolloClient} from 'apollo-client';
 import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
 
+const env = process.env.NODE_ENV;
+const uri = env === 'development' ? '/api/graphql' : 'https://feelsbox-server-v2.herokuapp.com/api/graphql';
+
 export default new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
         credentials: 'same-origin',
-        uri: '/api/graphql'
+        uri
     })
 });
