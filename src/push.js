fe4a41791135a8f27c6120d64460979c66cbb1ba
existdissolve <existdissolve@gmail.com>
2020-12-23 08:40:24 -0600

    maybe library is bad?

--- src/push.js
+++ src/push.js
@@ -1,11 +1,25 @@
 /*global Promise */
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
-import {decode} from 'base64-arraybuffer';
 
 import {subscribeToPush} from '-/graphql/user';
 import client from '-/graphql/client';
 
 const vapidKey = 'BN2FR0tXnjc4twlzmxAunsspOqjfB2n140ggTXTMOxhFHJJL-sW6ijc2K2ji3WbncJ81nfVxbzHLc1asz5WIQAE';
+
+const urlBase64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+};
 
 const saveSubscription = subscription => {
     return client.mutate({
@@ -18,21 +32,18 @@ const saveSubscription = subscription => {
 
 const configurePushSub = async() => {
     const sw = await navigator.serviceWorker.ready;
-    const key = decode(vapidKey);
+    const key = urlBase64ToUint8Array(vapidKey);
     let subscription = await sw.pushManager.getSubscription();
 
     if (subscription) {
         await subscription.unsubscribe();
     }
 
-    console.log(key)
-
     subscription = await sw.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: key
     });
 
-    console.log(subscription)
     await saveSubscription(subscription);
 };
 
