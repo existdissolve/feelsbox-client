f58880b6bd6890a80a0f2d5643251a328d98e51d
existdissolve <existdissolve@gmail.com>
2021-01-23 15:52:29 -0600

    Fix search functionality

--- src/components/feel/SearchGrid.jsx
+++ src/components/feel/SearchGrid.jsx
@@ -4,11 +4,19 @@ import {graphql} from 'react-apollo';
 import {compose} from 'recompose';
 import {withRouter} from 'react-router-dom';
 import GridList from '@material-ui/core/GridList';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AddBoxIcon from '@material-ui/icons/AddBox';
+import FlipToBackIcon from '@material-ui/icons/FlipToBack';
+import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
+import {Typography} from '@material-ui/core';
 import {get} from 'lodash';
 
 import Thumb from '-/components/feel/Thumb';
 import Loading from '-/components/Loading';
-import {getFeels} from '-/graphql/feel';
+import {copyFeel, getFeels, subscribe, unsubscribe} from '-/graphql/feel';
+import client from '-/graphql/client';
 
 const styles = theme => ({
     root: {
@@ -20,10 +28,125 @@ const styles = theme => ({
 });
 
 class SearchGrid extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            activeFeel: null,
+            anchorEl: null
+        };
+    }
+
+    onCopyClick = () => {
+        const {showSnackbar} = this.props;
+        const _id = get(this.state, 'activeFeel._id');
+
+        client.mutate({
+            mutation: copyFeel,
+            awaitRefetchQueries: true,
+            refetchQueries: [{
+                fetchPolicy: 'network-only',
+                query: getFeels
+            }],
+            variables: {_id}
+        }).then(() => {
+            showSnackbar('Added to My Feels!');
+        });
+
+        this.onMenuClose();
+    };
+
+    onMenuOpen = (anchorEl, activeFeel) => {
+        this.setState({
+            activeFeel,
+            anchorEl
+        });
+    };
+
+    onMenuClose = () => {
+        this.setState({
+            anchorEl: undefined
+        });
+    };
+
+    onSubscribeClick = () => {
+        const {showSnackbar} = this.props;
+        const _id = get(this.state, 'activeFeel._id');
+
+        client.mutate({
+            mutation: subscribe,
+            variables: {_id}
+        }).then(() => {
+            showSnackbar('Added to Favs!');
+        });
+
+        this.onMenuClose();
+    };
+
+    onUnsubscribeClick = () => {
+        const {showSnackbar} = this.props;
+        const _id = get(this.state, 'activeFeel._id');
+
+        client.mutate({
+            mutation: unsubscribe,
+            variables: {_id}
+        }).then(() => {
+            showSnackbar('Removed from Favs');
+        });
+
+        this.onMenuClose();
+    };
+
     render() {
+        const {activeFeel, anchorEl} = this.state;
         const {classes, showSnackbar} = this.props;
         const feels = get(this.props, 'data.feels', []);
         const loading = get(this.props, 'data.loading');
+        const menu = [];
+
+        if (activeFeel) {
+            const {isSubscribed, isSubscriptionOwner, name} = activeFeel;
+            const saveToFavs = (
+                <MenuItem onClick={this.onSubscribeClick} key="save_feel">
+                    <ListItemIcon>
+                        <AddBoxIcon />
+                    </ListItemIcon>
+                    Save to Favs
+                </MenuItem>
+            );
+            const removeFromFavs = (
+                <MenuItem onClick={this.onUnsubscribeClick} key="remove_feel">
+                    <ListItemIcon>
+                        <IndeterminateCheckBoxIcon />
+                    </ListItemIcon>
+                    Remove from Favs
+                </MenuItem>
+            );
+            const copyFeel = (
+                <MenuItem onClick={this.onCopyClick} key="copy_feel_save">
+                    <ListItemIcon>
+                        <FlipToBackIcon />
+                    </ListItemIcon>
+                    Copy to My Feels
+                </MenuItem>
+            );
+            const nameLabel = (
+                <MenuItem key="feel-name">
+                    <Typography variant="h6">
+                        {name}
+                    </Typography>
+                </MenuItem>
+            );
+
+            menu.push(nameLabel);
+            menu.push(copyFeel);
+
+            if (!isSubscribed) {
+                menu.push(saveToFavs);
+            } else if (isSubscriptionOwner) {
+                menu.push(removeFromFavs);
+            }
+        }
 
         return (
             <div className={classes.root}>
@@ -33,10 +156,20 @@ class SearchGrid extends React.Component {
                         {feels.map(feel => {
                             const {_id} = feel;
 
-                            return <Thumb feel={feel} key={_id} showSnackbar={showSnackbar} />;
+                            return (
+                                <Thumb
+                                    feel={feel}
+                                    key={_id}
+                                    displayMode="grid"
+                                    menuOpenHandler={this.onMenuOpen}
+                                    showSnackbar={showSnackbar} />
+                            );
                         })}
                     </GridList>
                 }
+                <Menu anchorEl={anchorEl} keepMounted={false} open={Boolean(anchorEl)} onClose={this.onMenuClose}>
+                    {menu}
+                </Menu>
             </div>
         );
     }
